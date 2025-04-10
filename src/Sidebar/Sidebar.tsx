47aa9b682145b5dee86173e70cec2fc68d3b93df
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-04-10 01:39:23 -0400

    sidebar css complete

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -22,29 +22,56 @@ function Sidebar() {
       background: '#FF69B4',
       width: '200px',
       height: '100vh',
-      // display: 'flex',
-      // flexDirection: 'column',
-      // alignItems: 'stretch'
-    },
-    logo: {
       display: 'flex',
-      justifyContent: 'space-evenly',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    logoAndTabs: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
     logoText: {
       color: 'white',
+    },
+    tabs: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'stretch'
+    },
+    profile: {
+      display: 'flex',
+      justifyContent: 'center',
+      background: 'white',
+      textAlign: 'center',
+      border: '1px solid black',
+      borderRadius: '0.5em',
+      marginBottom: '1em',
+    },
+    profileText: {
+      marginLeft: '1em',
+      marginRight: '1em',
+      marginTop: '0.5em',
+      marginBottom: '0.5em',
+      fontSize: '14px'
     }
   }
 
   return (
     <aside style={style.sideBarContainer}>
-      <div style={style.logo}>
+      <div style={style.logoAndTabs}>
         <h2 style={style.logoText}>
           Mirthaud
         </h2>
+        <div style={style.tabs}>
+          {allTabs}
+        </div>
       </div>
-      {allTabs}
-      <div>
-        Sign Out
+      <div style={style.profile}>
+        <h3 style={style.profileText}>
+          Sign Out
+        </h3>
       </div>
     </aside>
   )

fade18525c7bc65976e7e4c0811cd5308dd7f5c7
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-04-10 01:08:59 -0400

    sidebar buttons

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,15 +1,52 @@
+import SidebarButton from "../Buttons/SidebarButton"
+
 function Sidebar() {
 
-  const style: { [key:string]: React.CSSProperties} = {
+  const tabs = [
+    <SidebarButton buttonText="Home" />,
+    <SidebarButton buttonText="Interview" />,
+    <SidebarButton buttonText="Resume" />,
+    <SidebarButton buttonText="My Network" />,
+    <SidebarButton buttonText="Messaging" />]
+
+  const allTabs = tabs.map((tab, index) => {
+    return (
+      <div key={index}>
+        {tab}
+      </div>
+    )
+  })
+
+  const style: { [key: string]: React.CSSProperties } = {
     sideBarContainer: {
-      background: '#FF69B4'
+      background: '#FF69B4',
+      width: '200px',
+      height: '100vh',
+      // display: 'flex',
+      // flexDirection: 'column',
+      // alignItems: 'stretch'
+    },
+    logo: {
+      display: 'flex',
+      justifyContent: 'space-evenly',
+    },
+    logoText: {
+      color: 'white',
     }
   }
 
   return (
-    <div style={style.sideBarContainer}>
-      Home
-    </div>
+    <aside style={style.sideBarContainer}>
+      <div style={style.logo}>
+        <h2 style={style.logoText}>
+          Mirthaud
+        </h2>
+      </div>
+      {allTabs}
+      <div>
+        Sign Out
+      </div>
+    </aside>
   )
 }
 

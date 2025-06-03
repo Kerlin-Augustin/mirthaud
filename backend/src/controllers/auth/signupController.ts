5973cfe9e10167e6e2465dc366e1bf00284ffb41
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-06-03 14:32:54 -0400

    signup logic created

--- backend/src/controllers/auth/signupController.ts
+++ backend/src/controllers/auth/signupController.ts
@@ -1,5 +1,30 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import { UserModel } from '../../database/models/User.model';
+import bcrypt from 'bcrypt';
 
 export const signupController = async (req: Request, res: Response) => {
 
+  const { name, email, accountType, password } = req.body
+
+  let hashedPassword: string
+  let newUser;
+  const saltRounds = 10
+
+  try {
+    hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    newUser = new UserModel({
+      name,
+      email,
+      accountType,
+      password: hashedPassword
+    })
+
+    await newUser.save()
+    res.status(201).json({ message: 'Success' })
+
+  } catch (err) {
+    console.error('Failing to create user', err)
+    res.status(500).json({ error: 'Failed to create new user' })
+  }
 }
